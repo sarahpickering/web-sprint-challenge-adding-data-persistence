0c8ad5095854309d7ac154b65355d25f7fa5bc12
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-01-18 23:07:38 -0700

    finish tasks, update get model

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,33 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig')
+
+async function getTasks() {
+    const tasks = await db("tasks as t")
+      .join("projects as p", "t.project_id", "p.project_id")
+      .select(
+        "t.task_id",
+        "t.task_description",
+        "t.task_notes",
+        "t.task_completed",
+        "p.project_name",
+        "p.project_description"
+      )
+
+return tasks.map((task) => ({
+    ...task,
+    task_completed: Boolean(task.task_completed),
+  }));
+}
+
+async function addTask(task) {
+    const [task_id] = await db("tasks").insert(task);
+    const newTask = await db("tasks").where("task_id", task_id).first();
+    return {
+      ...newTask,
+      task_completed: Boolean(newTask.task_completed),
+    };
+  }
+  
+  module.exports = {
+    getTasks,
+    addTask,
+  };
